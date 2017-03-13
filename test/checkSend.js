1939b66a9a77ab56ff5fae488ce728952c2b1ad3
Tom Nash <contacttomnash@gmail.com>
2017-03-13 14:20:20 +0000

    Fix the incorrect send test

--- test/checkSend.js
+++ test/checkSend.js
@@ -9,7 +9,6 @@ let utils = require("./utils/utils.js")
 
 contract("Sending and receiving", function(accounts_) {
   it("should send coin correctly", co(function* () {
-    // Get initial balances of first and second account.
     let accOne = accounts_[0]
     let accTwo = accounts_[1]
     let amount = 10
@@ -23,21 +22,16 @@ contract("Sending and receiving", function(accounts_) {
     assert.equal(accTwoEndingBal.toNumber(), accTwoStartingBal.toNumber() + amount, "Amount wasn't correctly sent to the receiver")
   }))
   it("should not allow sending more than an account's balance", co(function* () {
-    // Get initial balances of first and second account.
     let accOne = accounts_[0]
     let accTwo = accounts_[1]
-    let accOneStartingBal
-    let accTwoStartingBal
-    let accOneEndingBal
-    let accTwoEndingBal
-    let amount = 10
+    let amount = 100
     let trst = yield utils.deployTrustcoin(accounts_[0], accounts_[1])
-    accOneStartingBal = yield trst.balanceOf.call(accOne)
-    accTwoStartingBal = yield trst.balanceOf.call(accTwo)
-    yield trst.transfer(accTwo, amount, {from: accOne})
-    accOneEndingBal = yield trst.balanceOf.call(accOne)
-    accTwoEndingBal = yield trst.balanceOf.call(accTwo)
-    assert.equal(accOneEndingBal.toNumber(), accOneStartingBal.toNumber() - amount, "Amount wasn't correctly taken from the sender")
-    assert.equal(accTwoEndingBal.toNumber(), accTwoStartingBal.toNumber() + amount, "Amount wasn't correctly sent to the receiver")
+    let accOneStartingBal = yield trst.balanceOf.call(accOne)
+    let accTwoStartingBal = yield trst.balanceOf.call(accTwo)
+    yield trst.transfer(accOne, accTwoStartingBal + amount, {from: accTwo})
+    let accOneEndingBal = yield trst.balanceOf.call(accOne)
+    let accTwoEndingBal = yield trst.balanceOf.call(accTwo)
+    assert.equal(accOneEndingBal.toNumber(), accOneStartingBal.toNumber(), "Amount was given to the receiver")
+    assert.equal(accTwoEndingBal.toNumber(), accTwoStartingBal.toNumber(), "Amount was taken from the sender")
   }))
 })
