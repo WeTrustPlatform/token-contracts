8d9212e78f9538619538e8984c4c621fbe64fb15
shinelay <aungshine>
2017-03-13 18:12:06 -0700

    removed unNecessary file

--- truffle.js
+++ truffle.js
@@ -5,10 +5,5 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     }
-  },
-  rpc: {
-    host: "localhost",
-    port: 8545,
-    network_id: "*" // Match any network id
   }
 };
