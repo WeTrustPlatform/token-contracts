62efcc9e41b00168de812201b41bd7b0002f21d1
Tom Nash <contacttomnash@gmail.com>
2017-03-14 16:50:35 +0000

    Be a little more stringent with assertions

--- test/checkAuth.js
+++ test/checkAuth.js
@@ -12,24 +12,32 @@ contract("Authentication", function(accounts_) {
     let migrationMaster = accounts_[1]
     let trst = yield utils.deployTrustcoin(owner, migrationMaster)
     yield utils.assertThrows(trst.changeMigrationMaster(owner, {from: owner}))
+    let contractMigrationMaster = yield trst.migrationMaster.call()
+    assert.equal(contractMigrationMaster, migrationMaster)
   }))
   it("should allow migration master to be changed by migration master", co(function* () {
     let owner = accounts_[0]
     let migrationMaster = accounts_[1]
     let trst = yield utils.deployTrustcoin(owner, migrationMaster)
     yield utils.assertDoesNotThrow(trst.changeMigrationMaster(owner, {from: migrationMaster}))
+    let contractMigrationMaster = yield trst.migrationMaster.call()
+    assert.equal(contractMigrationMaster, owner)
   }))
   it("should not allow migration period to be started by anyone other than migration master", co(function* () {
     let owner = accounts_[0]
     let migrationMaster = accounts_[1]
     let trst = yield utils.deployTrustcoin(owner, migrationMaster)
     yield utils.assertThrows(trst.beginMigrationPeriod(consts.NON_ZERO_ADDRESS, {from: owner}));
+    let allowMigrations = yield trst.allowOutgoingMigrations.call()
+    assert.equal(allowMigrations, false)
   }))
   it("should allow migration period to be started by migration master", co(function* () {
     let owner = accounts_[0]
     let migrationMaster = accounts_[1]
     let trst = yield utils.deployTrustcoin(owner, migrationMaster)
     yield utils.assertDoesNotThrow(trst.beginMigrationPeriod(consts.NON_ZERO_ADDRESS, {from: migrationMaster}))
+    let allowMigrations = yield trst.allowOutgoingMigrations.call()
+    assert.equal(allowMigrations, true)
   }))
   it("should not allow migration finalization by anyone other than migration master", co(function* () {
     let owner = accounts_[0]
@@ -38,6 +46,8 @@ contract("Authentication", function(accounts_) {
     yield trst.beginMigrationPeriod(consts.NON_ZERO_ADDRESS, {from: migrationMaster})
     utils.increaseTime(consts.ONE_YEAR_IN_SECONDS)
     yield utils.assertThrows(trst.finalizeOutgoingMigration({from: owner}))
+    let allowMigrations = yield trst.allowOutgoingMigrations.call()
+    assert.equal(allowMigrations, true)
   }))
   it("should allow migration finalization by migration master", co(function* () {
     let owner = accounts_[0]
@@ -47,5 +57,7 @@ contract("Authentication", function(accounts_) {
     yield trst.beginMigrationPeriod(trst2.address, {from: migrationMaster})
     utils.increaseTime(consts.ONE_YEAR_IN_SECONDS)
     yield utils.assertDoesNotThrow(trst.finalizeOutgoingMigration({from: migrationMaster}))
+    let allowMigrations = yield trst.allowOutgoingMigrations.call()
+    assert.equal(allowMigrations, false)
   }))
 })
