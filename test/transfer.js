9bb58e939d66d9173ce5c4ea6be964fc8bf1114d
Ron Merom <rmerom@gmail.com>
2017-04-04 15:36:00 -0700

    improve tests

--- test/transfer.js
+++ test/transfer.js
@@ -1,7 +1,8 @@
 'use strict'
 
-let co = require("co").wrap
 let assert = require('chai').assert
+let co = require("co").wrap
+let Promise = require("bluebird");
 let utils = require("./utils/utils.js")
 
 contract("Sending and receiving", function(accounts_) {
@@ -19,7 +20,24 @@ contract("Sending and receiving", function(accounts_) {
     let trst = yield utils.deployTrustcoin(OWNER, MIGRATION_MASTER)
     let accountOneStartingBalance = yield trst.balanceOf.call(OWNER)
     let accountTwoStartingBalance = yield trst.balanceOf.call(MIGRATION_MASTER)
+
+    let transferEventFired = false
+    let transferEvent = trst.Transfer();  // eslint-disable-line new-cap
+    transferEvent.watch(function(error, log) {
+      assert.isNotOk(error)
+      transferEvent.stopWatching();
+      transferEventFired = true;
+      assert.equal(log.args.from, OWNER);
+      assert.equal(log.args.to, MIGRATION_MASTER);
+      assert.equal(log.args.value.toNumber(), amount);
+    });
+
     yield trst.transfer(MIGRATION_MASTER, amount, {from: OWNER})
+
+    // A bit of a hack to catch the event.
+    yield Promise.delay(300);
+    assert.isOk(transferEventFired)
+
     let accountOneEndingBalance = yield trst.balanceOf.call(OWNER)
     let accountTwoEndingBalance = yield trst.balanceOf.call(MIGRATION_MASTER)
     assert.equal(accountOneEndingBalance.toNumber(), accountOneStartingBalance.toNumber() - amount)
@@ -33,7 +51,19 @@ contract("Sending and receiving", function(accounts_) {
     let trst = yield utils.deployTrustcoin(OWNER, MIGRATION_MASTER)
     let accountOneStartingBalance = yield trst.balanceOf.call(OWNER)
     let accountTwoStartingBalance = yield trst.balanceOf.call(MIGRATION_MASTER)
+
+    let transferEvent = trst.Transfer();  // eslint-disable-line new-cap
+    transferEvent.watch(function(error, log) {
+      transferEvent.stopWatching();
+      assert.isNotOk(true, "Transfer event fired when it shouldn't have")
+    });
+
     yield trst.transfer(OWNER, accountTwoStartingBalance + amount, {from: MIGRATION_MASTER})
+
+    // Give some time for the testrpc to process the tx and make sure no event has fired.
+    Promise.delay(300)
+    transferEvent.stopWatching()
+
     let accountOneEndingBalance = yield trst.balanceOf.call(OWNER)
     let accountTwoEndingBalance = yield trst.balanceOf.call(MIGRATION_MASTER)
     assert.equal(accountOneEndingBalance.toNumber(), accountOneStartingBalance.toNumber())
