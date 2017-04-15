2ecb368dc62d1a0b9532b18b279544bb0a12f4e8
shine2lay <aungshine@gmail.com>
2017-04-14 17:01:06 -0700

    fix issues from slack

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-let fs = require(‘fs’);
-let solc = require(‘solc’);
-let co = require(‘co’).wrap
-let path = require(‘path’)
+let fs = require('fs');
+let solc = require('solc');
+let co = require('co').wrap
+let path = require('path')
 
-let trustcoinSourcePath = path.join(__dirname, ‘../../contracts/Trustcoin.sol’)
-let trustcoinDepPath = path.join(__dirname, ‘../../contracts/deps/ERC20TokenInterface.sol’)
+let trustcoinSourcePath = path.join(__dirname, '../../contracts/Trustcoin.sol')
+let trustcoinDepPath = path.join(__dirname, '../../contracts/deps/ERC20TokenInterface.sol')
 
-const SOLC_VERSION = ‘v0.4.8+commit.60cc1668’
+const SOLC_VERSION = 'v0.4.8+commit.60cc1668'
 
 function getContractContents(filepath) {
-    let content = fs.readFileSync(filepath, ‘utf8’);
+    let content = fs.readFileSync(filepath, 'utf8');
     return content;
 }
 
 function compileSource (input, callback) {
     solc.loadRemoteVersion(SOLC_VERSION, function(e, correctSolc) {
         if (e) {
-            throw new Error(‘Could not load correct solc version’, e)
+            throw new Error('Could not load correct solc version', e)
         }
         let output = correctSolc.compile({sources: input}, 1)
         callback(null, output)
@@ -25,8 +25,8 @@ function compileSource (input, callback) {
 
 let main = co(function*() {
     let compilerInput = {
-    ‘Trustcoin.sol’: getContractContents(trustcoinSourcePath),
-    ‘deps/ERC20TokenInterface.sol’: getContractContents(trustcoinDepPath)
+    'Trustcoin.sol': getContractContents(trustcoinSourcePath),
+    'deps/ERC20TokenInterface.sol': getContractContents(trustcoinDepPath)
 }
 
     let compiledOutput = yield new Promise((resolve, reject) => {
