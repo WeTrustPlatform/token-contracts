1db641cbd28f27f54a53ee52344d752d97bb300d
shine2lay <aungshine@gmail.com>
2017-04-17 20:24:33 -0700

    index.js fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let fs = require('fs');
 let solc = require('solc');
-let cowrap = require('co')
+let co = require('co')
 let path = require('path')
 
 let trustcoinSourcePath = path.join(__dirname, './contracts/Trustcoin.sol')
@@ -9,32 +9,33 @@ let trustcoinDepPath = path.join(__dirname, './contracts/deps/ERC20TokenInterfac
 const SOLC_VERSION = 'v0.4.8+commit.60cc1668'
 
 function getContractContents(filepath) {
-  let content = fs.readFileSync(filepath, 'utf8');
-  return content;
+    let content = fs.readFileSync(filepath, 'utf8');
+    return content;
 }
 
 function compileSource (input, callback) {
-  return new Promise((resolve, reject) => {
-    solc.loadRemoteVersion(SOLC_VERSION, cowrap(function*(e, correctSolc) {
-      if (e) {
-        throw new Error('Could not load correct solc version', e)
-      }
-      let output = correctSolc.compile({sources: input}, 1)
-      resolve(output)
-    }))
-  })
+    return new Promise((resolve, reject) => {
+        solc.loadRemoteVersion(SOLC_VERSION, function(e, correctSolc) {
+            if (e) {
+                throw new Error('Could not load correct solc version', e)
+            }
+            let output = correctSolc.compile({sources: input}, 1)
+            resolve(output)
+        })
+    })
 }
 
-let main = cowrap(function*() {
-  let compilerInput = {
-    'Trustcoin.sol': getContractContents(trustcoinSourcePath),
-    'deps/ERC20TokenInterface.sol': getContractContents(trustcoinDepPath)
-  }
-
-  let compiledOutput = yield compileSource(compilerInput)
-  let contractOutput = compiledOutput.contracts.Trustcoin
-  let trustcoinContractAbi = JSON.parse(contractOutput.interface);
-  return {abi: trustcoinContractAbi, bytecode: contractOutput.bytecode};
-})
+let main = {
+    getContract: co.wrap(function*() {
+        let compilerInput = {
+            'Trustcoin.sol': getContractContents(trustcoinSourcePath),
+            'deps/ERC20TokenInterface.sol': getContractContents(trustcoinDepPath)
+        }
+        let compiledOutput = yield compileSource(compilerInput)
+        let contractOutput = compiledOutput.contracts.Trustcoin
+        let trustcoinContractAbi = JSON.parse(contractOutput.interface);
+        return {abi: trustcoinContractAbi, bytecode: contractOutput.bytecode};
+    })
+}
 
 module.exports = main
